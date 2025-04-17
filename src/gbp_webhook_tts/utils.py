72efa43d41456a6ff0c39052d615c71ea1ab6142
Albert Hopkins <marduk@letterboxes.org>
2025-04-16 20:32:57 -0500

    utils.event_to_speech split dashes
    Split dashes into seperate workds so, e.g. "kde-desktop" becomes "kde
    desktop". This will hopefully help the TTS engine with pronunciation.

--- src/gbp_webhook_tts/utils.py
+++ src/gbp_webhook_tts/utils.py
@@ -27,8 +27,8 @@ def event_to_path(event: dict[str, Any]) -> Path:
 
 def event_to_speech(event: dict[str, Any]) -> bytes:
     """Return .mp3 audio for the given event"""
-    machine = event["machine"]
+    text = event["machine"].replace("-", " ")
     polly = boto3.Session().client("polly")
-    response = polly.synthesize_speech(VoiceId="Ivy", OutputFormat="mp3", Text=machine)
+    response = polly.synthesize_speech(VoiceId="Ivy", OutputFormat="mp3", Text=text)
 
     return response["AudioStream"].read()
