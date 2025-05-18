13d08b7e6992ec3eea744772f823cfb07f71caca
Albert Hopkins <marduk@letterboxes.org>
2025-05-18 06:46:39 -0500

    handlers: use with on Popen
    To silence pylint and to ensure the process get's wait()ed.

--- src/gbp_webhook_tts/handlers.py
+++ src/gbp_webhook_tts/handlers.py
@@ -11,4 +11,5 @@ def build_pulled(event: dict[str, Any]) -> None:
     sound_file = utils.acquire_sound_file(event)
     args = ["pw-play", str(sound_file)]
 
-    sp.Popen(args)  # pylint: disable=consider-using-with
+    with sp.Popen(args):
+        pass
