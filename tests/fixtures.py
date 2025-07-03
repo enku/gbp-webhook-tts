266518a522d1bffe66c9c7dd5884affb255c1753
Albert Hopkins <marduk@letterboxes.org>
2025-07-03 11:36:44 -0500

    tests.fixtures: rename FixtureContext => FC

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -10,13 +10,14 @@ from unittest_fixtures import FixtureContext, Fixtures, fixture
 
 from gbp_webhook_tts import handlers, utils
 
+FC = FixtureContext
 Mock = mock.Mock
 
 
 @fixture()
 def environ(
     _: Fixtures, *, environ: Mapping[str, str] | None = None
-) -> FixtureContext[MutableMapping[str, str]]:
+) -> FC[MutableMapping[str, str]]:
     environ = environ or {}
 
     with mock.patch.dict(os.environ, clear=True):
@@ -25,40 +26,36 @@ def environ(
 
 
 @fixture()
-def tmpdir(_: Fixtures) -> FixtureContext[Path]:
+def tmpdir(_: Fixtures) -> FC[Path]:
     with tempfile.TemporaryDirectory() as tempdir:
         yield Path(tempdir)
 
 
 @fixture()
-def popen(_: Fixtures, target: Module = handlers.sp) -> FixtureContext[Mock]:
+def popen(_: Fixtures, target: Module = handlers.sp) -> FC[Mock]:
     with mock.patch.object(target, "Popen") as mock_obj:
         yield mock_obj
 
 
 @fixture()
-def acquire_sound_file(
-    _: Fixtures, target: Module = handlers.utils
-) -> FixtureContext[Mock]:
+def acquire_sound_file(_: Fixtures, target: Module = handlers.utils) -> FC[Mock]:
     with mock.patch.object(target, "acquire_sound_file") as mock_obj:
         yield mock_obj
 
 
 @fixture()
-def user_cache_path(
-    _: Fixtures, target: Module = utils.platformdirs
-) -> FixtureContext[Mock]:
+def user_cache_path(_: Fixtures, target: Module = utils.platformdirs) -> FC[Mock]:
     with mock.patch.object(target, "user_cache_path") as mock_obj:
         yield mock_obj
 
 
 @fixture()
-def event_to_speech(_: Fixtures, target: Module = utils) -> FixtureContext[Mock]:
+def event_to_speech(_: Fixtures, target: Module = utils) -> FC[Mock]:
     with mock.patch.object(target, "event_to_speech") as mock_obj:
         yield mock_obj
 
 
 @fixture()
-def boto3_session(_: Fixtures, target: Module = utils.boto3) -> FixtureContext[Mock]:
+def boto3_session(_: Fixtures, target: Module = utils.boto3) -> FC[Mock]:
     with mock.patch.object(target, "Session") as mock_obj:
         yield mock_obj
