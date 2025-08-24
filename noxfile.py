42297d0f7fe468b3c968312d534526d8e95fa382
Albert Hopkins <marduk@letterboxes.org>
2025-08-23 20:24:36 -0500

    noxfile: remove Python 3.14 from tests
    pydispatch is still not working with 3.14. Disable for now.

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@
 import nox
 
 
-@nox.session(python=("3.12", "3.13", "3.14"))
+@nox.session(python=("3.12", "3.13"))
 def tests(session: nox.Session) -> None:
     dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
         "dev"
